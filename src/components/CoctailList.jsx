64ec6022a1f5c6e7a7ad5513a6f1b0fbe3fcc0ef
Jaroslaw Charchula <charchula@gmail.com>
2022-08-19 21:32:04 +0200

    fix name typo edit css edit context edit home and cocktaillist

--- src/components/CoctailList.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CoctailList = () => {
-  return <div>CoctailList</div>;
-};
-
-export default CoctailList;
--- src/context.js
+++ src/context.js
@@ -1,10 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
+
+  return (
+    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
