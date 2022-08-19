172ef5b4f9c3be57eb023ba6f8f81e3cefab5f16
Jaroslaw Charchula <charchula@gmail.com>
2022-08-19 17:22:56 +0200

    set About set css for about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+        voluptate reiciendis asperiores, veniam nihil eligendi reprehenderit et
+        culpa tenetur quibusdam itaque suscipit, quisquam pariatur ad natus nam
+        saepe ullam aperiam quae! Dicta veniam magnam tempora quo pariatur
+        ducimus vitae accusamus.
+      </p>
+    </section>
+  );
 };
 
 export default About;
