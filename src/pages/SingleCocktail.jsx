f15698c1b91527b2e2fccff50281800b6a3cc40e
Jaroslaw Charchula <charchula@gmail.com>
2022-08-19 17:00:27 +0200

    page name fix set react router in app

--- src/pages/SingleCocktail.jsx
+++ src/pages/SingleCocktail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const SingleCoctail = () => {
+const SingleCocktail = () => {
   return <div>SingleCoctail</div>;
 };
 
-export default SingleCoctail;
+export default SingleCocktail;
