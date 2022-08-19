b6ba0594351293efdf1401e776e0d29cbb4c1d0d
Jaroslaw Charchula <charchula@gmail.com>
2022-08-19 17:13:26 +0200

    edit Navbar add navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../img/logo.svg';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='cocktail db logo' className='logo' />
+        </Link>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>home</Link>
+          </li>
+          <li>
+            <Link to='/about'>about</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
